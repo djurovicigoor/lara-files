1ec5581cb53850bb009ea9c08e86ce5122de9bbb
djurovic <djurovic.igoor@gmail.com>
2017-11-20 19:42:43 +0100

    ServiceProvider

---
+++ src/Providers/LaraFilesProvider.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: djurovic
+ * Date: 20.11.17.
+ * Time: 19.24
+ */
+
+namespace DjurovicIgoor\LaraFiles\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraFilesProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		//
+	}
+}
