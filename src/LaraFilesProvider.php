94170ddd18669085d60e1371d65b90eb2181a42f
Igor <djurovic.igoor@gmail.com>
2019-04-17 14:05:42 +0200

    Pre Install script, aws driver added to composer.json

--- src/LaraFilesProvider.php
+++ src/LaraFilesProvider.php
@@ -8,32 +8,44 @@
 
 namespace DjurovicIgoor\LaraFiles;
 
+use DjurovicIgoor\LaraFiles\Commands\PreInstallCheck;
 use Illuminate\Support\ServiceProvider;
 
 class LaraFilesProvider extends ServiceProvider {
-
-	/**
-	 * Bootstrap the application services.
-	 *
-	 * @return void
-	 */
-	public function boot() {
-
-		$this->publishes([
-			__DIR__ . '/config/lara-files.php' => config_path('lara-files.php'),
-		], 'config');
-		$this->publishes([
-			__DIR__ . '/database/migrations/' => base_path('/database/migrations'),
-		], 'migrations');
-	}
-
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register() {
-
-		$this->mergeConfigFrom(__DIR__ . '/config/lara-files.php', 'lara-files');
-	}
+    
+    /**
+     * Bootstrap the application services.
+     * @return void
+     */
+    public function boot() {
+        
+        $this->publishes([
+            __DIR__ . '/config/lara-files.php' => config_path('lara-files.php'),
+        ], 'config');
+        $this->publishes([
+            __DIR__ . '/database/migrations/' => base_path('/database/migrations'),
+        ], 'migrations');
+    }
+    
+    /**
+     * Register the application services.
+     * @return void
+     */
+    public function register() {
+        
+        $this->commands([
+            PreInstallCheck::class,
+        ]);
+        $this->mergeConfigFrom(__DIR__ . '/config/lara-files.php', 'lara-files');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function provides() {
+        
+        return [
+            PreInstallCheck::class,
+        ];
+    }
 }
