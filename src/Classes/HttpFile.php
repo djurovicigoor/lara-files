38a0ed5f7055407ff8322ebb3050fa136f03d9b3
Igor <djurovic.igoor@gmail.com>
2019-04-18 09:51:43 +0200

    Con tract added, Custom file clasess prepared

---
+++ src/Classes/HttpFile.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: djurovic
+ * Date: 2019-04-18
+ * Time: 09:19
+ */
+
+namespace DjurovicIgoor\LaraFiles\Classes;
+
+use DjurovicIgoor\LaraFiles\Contracts\MoveFileInterfaces;
+
+class HttpFile implements MoveFileInterfaces {
+    
+    public function move() {
+        // TODO: Implement move() method.
+    }
+}
---
+++ src/Contracts/MoveFileInterfaces.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: djurovic
+ * Date: 2019-04-18
+ * Time: 08:58
+ */
+
+namespace DjurovicIgoor\LaraFiles\Contracts;
+
+interface MoveFileInterfaces {
+    
+    public function move();
+    
+}
