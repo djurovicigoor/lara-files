fd911f661f4eff5af63807a9a4d2d63fa00500f3
Djurović Igor <djurovic.igoor@gmail.com>
2022-09-12 09:39:03 +0200

    Better handle autoload relations

--- src/Traits/LaraFileTrait.php
+++ src/Traits/LaraFileTrait.php
@@ -63,23 +63,22 @@ trait LaraFileTrait
      */
     public function getRelationValue($key)
     {
-
-        // If the key already exists in the relationships array, it just means the
-        // relationship has already been loaded, so we'll just return it out of
-        // here because there is no need to query within the relations twice.
         if ($this->relationLoaded($key)) {
             return $this->relations[ $key ];
         }
+        
+        if (!$this->isRelation($key) && !(in_array($key, config('lara-files.types')) || in_array(str_singular($key), config('lara-files.types')))) {
+            return;
+        }
+        
+        if ($this->preventsLazyLoading) {
+            $this->handleLazyLoadingViolation($key);
+        }
+        
         // If the "attribute" exists as a method on the model, we will just assume
         // it is a relationship and will load and return results from the query
-        // and hydrate the relationship's value on the "relationships" array. || in_array($key, $this->morphs)
-        if (in_array($key, config('lara-files.types')) || in_array(str_singular($key), config('lara-files.types'))) {
-            return $this->getRelationshipFromMethod($key);
-        } else {
-            if (method_exists($this, $key)) {
-                return $this->getRelationshipFromMethod($key);
-            }
-        }
+        // and hydrate the relationship's value on the "relationships" array.
+        return $this->getRelationshipFromMethod($key);
     }
 
     /**
