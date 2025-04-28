581799778f9010122dad7ce490b84546d02f76d3
djurovicigoor <15280567+djurovicigoor@users.noreply.github.com>
2025-04-28 18:27:46 +0000

    Apply automatic changes

--- src/Classes/LaraFileUploader.php
+++ src/Classes/LaraFileUploader.php
@@ -2,19 +2,19 @@
 
 namespace DjurovicIgoor\LaraFiles\Classes;
 
-use Throwable;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Storage;
-use DjurovicIgoor\LaraFiles\Models\LaraFile;
 use DjurovicIgoor\LaraFiles\Contracts\UploadFileInterface;
-use Illuminate\Contracts\Filesystem\FileNotFoundException;
-use DjurovicIgoor\LaraFiles\Exceptions\UnableToUploadFileException;
-use DjurovicIgoor\LaraFiles\Exceptions\VisibilityIsNotValidException;
-use DjurovicIgoor\LaraFiles\Exceptions\FileTypeIsNotPresentedException;
-use DjurovicIgoor\LaraFiles\Exceptions\UnsupportedDiskAdapterException;
 use DjurovicIgoor\LaraFiles\Exceptions\FileIsNotBase64EncodedStringException;
 use DjurovicIgoor\LaraFiles\Exceptions\FileIsNotInstanceOfLaraFileModelException;
 use DjurovicIgoor\LaraFiles\Exceptions\FileIsNotInstanceOfUploadedFileClassException;
+use DjurovicIgoor\LaraFiles\Exceptions\FileTypeIsNotPresentedException;
+use DjurovicIgoor\LaraFiles\Exceptions\UnableToUploadFileException;
+use DjurovicIgoor\LaraFiles\Exceptions\UnsupportedDiskAdapterException;
+use DjurovicIgoor\LaraFiles\Exceptions\VisibilityIsNotValidException;
+use DjurovicIgoor\LaraFiles\Models\LaraFile;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Throwable;
 
 /**
  * Class LaraFileUploader
@@ -26,19 +26,19 @@ use DjurovicIgoor\LaraFiles\Exceptions\FileIsNotInstanceOfUploadedFileClassExcep
 class LaraFileUploader
 {
     private UploadFileInterface $uploadedFile;
-    
+
     private ?string $disk = null;
-    
+
     private ?string $type = null;
-    
+
     private ?string $visibility = null;
-    
+
     private ?string $name = null;
-    
+
     private array $customProperties = [];
-    
+
     private ?Model $model = null;
-    
+
     /**
      * Constructor to initialize the uploaded file based on the type of file uploader provided.
      *
@@ -59,7 +59,7 @@ class LaraFileUploader
             $this->uploadedFile = new AnotherLaraFile($uploadedFile);
         }
     }
-    
+
     /**
      * @return LaraFileUploader
      *
@@ -68,31 +68,31 @@ class LaraFileUploader
     public function setType(string $type): static
     {
         \throw_if(! $type || $type == '', new FileTypeIsNotPresentedException);
-        
+
         $this->type = $type;
-        
+
         return $this;
     }
-    
+
     /**
      * @return LaraFileUploader
      */
     public function setModel(Model $model): static
     {
         $this->model = $model;
-        
+
         return $this;
     }
-    
+
     public function getVisibility(): string
     {
-        if ( ! $this->visibility) {
+        if (! $this->visibility) {
             return \config('lara-files.visibility');
         }
-        
+
         return $this->visibility;
     }
-    
+
     /**
      * @return LaraFileUploader
      *
@@ -101,21 +101,21 @@ class LaraFileUploader
     public function setVisibility(string $visibility): static
     {
         \throw_if(! in_array($visibility, ['public', 'private']), new VisibilityIsNotValidException($visibility));
-        
+
         $this->visibility = $visibility;
-        
+
         return $this;
     }
-    
+
     public function getDisk(): string
     {
-        if ( ! $this->disk) {
+        if (! $this->disk) {
             return \config('lara-files.default_disk');
         }
-        
+
         return $this->disk;
     }
-    
+
     /**
      * @return LaraFileUploader
      *
@@ -124,90 +124,90 @@ class LaraFileUploader
     public function setDisk(string $disk): static
     {
         throw_if(! array_key_exists($disk, config('filesystems.disks')), new UnsupportedDiskAdapterException($disk));
-        
+
         $this->disk = $disk;
-        
+
         return $this;
     }
-    
+
     /**
      * @return LaraFileUploader
      */
     public function setName(string $name): static
     {
         $this->name = $name;
-        
+
         return $this;
     }
-    
+
     public function setCustomProperties(array $customProperties): self
     {
         $this->customProperties = $customProperties;
-        
+
         return $this;
     }
-    
+
     /**
      * @throws UnsupportedDiskAdapterException|FileTypeIsNotPresentedException|FileNotFoundException|UnableToUploadFileException|Throwable
      */
     public function upload(): LaraFile
     {
         \throw_if(! $this->getDisk(), new UnsupportedDiskAdapterException($this->getDisk()));
-        
+
         \throw_if(! $this->type, new FileTypeIsNotPresentedException);
-        
-        $fileExtension    = $this->uploadedFile->getFileExtension();
+
+        $fileExtension = $this->uploadedFile->getFileExtension();
         $fileOriginalName = $this->name ?? $this->uploadedFile->getFileOriginalName();
-        $fileHashName     = $this->uploadedFile->getHashName();
-        
+        $fileHashName = $this->uploadedFile->getHashName();
+
         if ($this->model instanceof Model) {
             $path = 'lara-files/'.strtolower(class_basename($this->model));
         } else {
             $path = 'lara-files/tmp';
         }
-        
+
         $fullPath = "$path/$fileHashName.$fileExtension";
-        
+
         $isSuccessfullyUploaded = Storage::disk($this->getDisk())->put($fullPath, $this->uploadedFile->getFileForUpload(), [
-                'visibility' => $this->getVisibility(),
+            'visibility' => $this->getVisibility(),
         ]);
-        
+
         \throw_if(! $isSuccessfullyUploaded, new UnableToUploadFileException);
-        
+
         $laraFile = new LaraFile([
-                'disk'       => $this->getDisk(), 'path' => $path, 'hash_name' => $fileHashName, 'extension' => $fileExtension, 'name' => $fileOriginalName, 'type' => $this->type,
-                'visibility' => $this->getVisibility(), 'custom_properties' => $this->customProperties,
+            'disk' => $this->getDisk(), 'path' => $path, 'hash_name' => $fileHashName, 'extension' => $fileExtension, 'name' => $fileOriginalName, 'type' => $this->type,
+            'visibility' => $this->getVisibility(), 'custom_properties' => $this->customProperties,
         ]);
-        
+
         if ($this->model instanceof Model) {
             $laraFile->larafilesable()->associate($this->model);
         }
-        
-        if ( ! $laraFile->save()) {
+
+        if (! $laraFile->save()) {
             if (Storage::disk($this->getDisk())->exists($fullPath)) {
                 Storage::disk($this->getDisk())->delete($fullPath);
             }
             throw new UnableToUploadFileException;
         }
-        
+
         return $laraFile;
     }
-    
+
     /**
      * @throws FileNotFoundException
      * @throws Throwable
      */
     public static function uploadForOptimizationAndManipulation(
-            $uploadedFile,
-            $fileUploaderType,
-            $type,
-            $disk = null,
-            $visibility = null,
-            $name = null,
-            array $customProperties = []
+        $uploadedFile,
+        $fileUploaderType,
+        $type,
+        $disk = null,
+        $visibility = null,
+        $name = null,
+        array $customProperties = []
     ): LaraFile {
         $laraFileUploader = (new LaraFileUploader(uploadedFile: $uploadedFile, fileUploaderType: $fileUploaderType))->setType(type: $type);
-        
+
         if ($disk) {
             $laraFileUploader->setDisk(disk: $disk);
         }
@@ -220,7 +220,7 @@ class LaraFileUploader
         if (\count($customProperties)) {
             $laraFileUploader->setCustomProperties(customProperties: $customProperties);
         }
-        
+
         return $laraFileUploader->upload();
     }
 }
--- tests/Unit/LaraFileUploaderTest.php
+++ tests/Unit/LaraFileUploaderTest.php
@@ -1,12 +1,12 @@
 <?php
 
+use DjurovicIgoor\LaraFiles\Classes\LaraFileUploader;
+use DjurovicIgoor\LaraFiles\Exceptions\FileTypeIsNotPresentedException;
+use DjurovicIgoor\LaraFiles\Exceptions\UnsupportedDiskAdapterException;
+use DjurovicIgoor\LaraFiles\Exceptions\VisibilityIsNotValidException;
+use Djurovicigoor\LaraFiles\Tests\TestSupport\TestModels\TestModel;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
-use DjurovicIgoor\LaraFiles\Classes\LaraFileUploader;
-use Djurovicigoor\LaraFiles\Tests\TestSupport\TestModels\TestModel;
-use DjurovicIgoor\LaraFiles\Exceptions\VisibilityIsNotValidException;
-use DjurovicIgoor\LaraFiles\Exceptions\UnsupportedDiskAdapterException;
-use DjurovicIgoor\LaraFiles\Exceptions\FileTypeIsNotPresentedException;
 
 beforeEach(function () {
     Storage::fake('local');
@@ -14,46 +14,46 @@ beforeEach(function () {
 
 it('uploads a file without model in local storage using package main class', function () {
     $file = UploadedFile::fake()->image('avatar.jpg');
-    
+
     $uploadedFile = (new LaraFileUploader($file))->setDisk('local')->setType('avatar')->upload();
-    
+
     Storage::disk('local')->assertExists($uploadedFile->data_path);
-    
+
     $this->assertDatabaseHas('lara_files', [
-            'id' => $uploadedFile->id,
+        'id' => $uploadedFile->id,
     ]);
 });
 
 it('uploads a file and attach it to test model using package main class', function () {
-    $file      = UploadedFile::fake()->image('avatar.jpg');
+    $file = UploadedFile::fake()->image('avatar.jpg');
     $testModel = TestModel::first();
-    
+
     $uploadedFile = (new LaraFileUploader($file))->setDisk('local')->setType('avatar')->setModel($testModel)->upload();
-    
+
     Storage::disk('local')->assertExists($uploadedFile->data_path);
-    
+
     $this->assertDatabaseHas('lara_files', [
-            'id' => $uploadedFile->id, 'larafilesable_type' => TestModel::class, 'larafilesable_id' => $testModel->id,
+        'id' => $uploadedFile->id, 'larafilesable_type' => TestModel::class, 'larafilesable_id' => $testModel->id,
     ]);
 });
 
 it('fails to upload file with invalid visibility', function () {
-    $file      = UploadedFile::fake()->image('avatar.jpg');
-    
+    $file = UploadedFile::fake()->image('avatar.jpg');
+
     (new LaraFileUploader($file))->setVisibility('foo');
-    
+
 })->throws(VisibilityIsNotValidException::class);
 
 it('fails to upload file with invalid disk', function () {
-    $file      = UploadedFile::fake()->image('avatar.jpg');
-    
+    $file = UploadedFile::fake()->image('avatar.jpg');
+
     (new LaraFileUploader($file))->setDisk('foo');
-    
+
 })->throws(UnsupportedDiskAdapterException::class);
 
 it('fails to upload file with empty type', function () {
-    $file      = UploadedFile::fake()->image('avatar.jpg');
-    
+    $file = UploadedFile::fake()->image('avatar.jpg');
+
     (new LaraFileUploader($file))->setType('');
-    
+
 })->throws(FileTypeIsNotPresentedException::class);
